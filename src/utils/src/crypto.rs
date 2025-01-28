02f48c4fc991f54ecafd1ff33bd3cfd46da1a79e
Elham Aryanpur <elhamaryanpur5@gmail.com>
2025-01-28 23:54:48 +0300

    feat: added base64 support

--- src/utils/src/crypto.rs
+++ src/utils/src/crypto.rs
@@ -1,8 +1,13 @@
 // Cryptography util. Currently supporting only SHA2 and SHA3 (256 and 512 variants)
 
+use base64::{
+    prelude::{BASE64_STANDARD, BASE64_URL_SAFE},
+    Engine,
+};
+
 pub struct LuaCrypto {}
 impl LuaCrypto {
-    pub fn hash(hash_type: String, input: String) -> String {
+    fn hash(hash_type: String, input: String) -> String {
         macro_rules! sha_impl {
             ($hash_function:ty) => {
                 let mut sha = <$hash_function>::new();
@@ -33,13 +38,66 @@ impl LuaCrypto {
             "".to_string()
         }
     }
+
+    fn base64_encode(input: String) -> String {
+        let mut output_buf = String::new();
+        BASE64_STANDARD.encode_string(input, &mut output_buf);
+        output_buf
+    }
+
+    fn base64_encode_urlsafe(input: String) -> String {
+        let mut output_buf = String::new();
+        BASE64_URL_SAFE.encode_string(input, &mut output_buf);
+        output_buf
+    }
+
+    fn base64_decode(input: String) -> mlua::Result<String> {
+        let mut output_buf = Vec::new();
+        match BASE64_STANDARD.decode_slice(input.as_bytes(), &mut output_buf) {
+            Ok(_) => Ok(String::from_utf8_lossy(output_buf.as_slice()).to_string()),
+            Err(e) => Err(mlua::Error::runtime(format!(
+                "Could not decode the base64 encoded input: {e:?}"
+            ))),
+        }
+    }
+
+    fn base64_decode_urlsafe(input: String) -> mlua::Result<String> {
+        let mut output_buf = Vec::new();
+        match BASE64_URL_SAFE.decode_slice(input.as_bytes(), &mut output_buf) {
+            Ok(_) => Ok(String::from_utf8_lossy(output_buf.as_slice()).to_string()),
+            Err(e) => Err(mlua::Error::runtime(format!(
+                "Could not decode the base64 encoded input: {e:?}"
+            ))),
+        }
+    }
 }
 impl crate::LuaUtils for LuaCrypto {
     async fn register_to_lua(lua: &mlua::Lua) -> mlua::Result<()> {
         let hash_function = lua.create_function(|_, (hash_type, input): (String, String)| {
             Ok(Self::hash(hash_type, input))
         })?;
+        lua.globals().set("hash", hash_function)?;
 
-        lua.globals().set("hash", hash_function)
+        lua.globals().set(
+            "base64_encode",
+            lua.create_function(|_, input: String| Ok(Self::base64_encode(input)))?,
+        )?;
+
+        lua.globals().set(
+            "base64_encode_urlsafe",
+            lua.create_function(|_, input: String| Ok(Self::base64_encode_urlsafe(input)))?,
+        )?;
+
+        lua.globals().set(
+            "base64_decode",
+            lua.create_function(|_, input: String| Self::base64_decode(input))?,
+        )?;
+
+        lua.globals().set(
+            "base64_decode_urlsafe",
+            lua.create_function(|_, input: String| Self::base64_decode_urlsafe(input))?,
+        )?;
+
+        Ok(())
     }
 }
