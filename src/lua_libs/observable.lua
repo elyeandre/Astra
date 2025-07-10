d165b53c0435b04a4ff223d0b5f01afc6b2b6657
Khush Mehta <130106727+khushmmehta@users.noreply.github.com>
2025-07-10 05:57:53 +0530

    refactor: add type hints

--- src/lua_libs/observable.lua
+++ src/lua_libs/observable.lua
@@ -1,13 +1,20 @@
+---
+---@param val any
+---@return table
 Observable = function(val)
 	local new_observable = {
 		value = val,
 		observers = {},
 	}
 
+	---Subscribe to an observable object with a callback function
+	---@param observer function
 	function new_observable:subscribe(observer)
 		table.insert(self.observers, observer)
 	end
 
+	---Unsubscribe a callback function from an observable object
+	---@param observer function
 	function new_observable:unsubscribe(observer)
 		for i = #self.observers, 1, -1 do
 			if self.observers[i] == observer then
@@ -16,6 +23,8 @@ Observable = function(val)
 		end
 	end
 
+	---
+	---@param data any
 	function new_observable:publish(data)
 		for i = 1, #self.observers do
 			self.observers[i](data)
