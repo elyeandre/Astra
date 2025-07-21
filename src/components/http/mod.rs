31dff8017671b6f90c7852a16e01bb93a29c54de
Elham Aryanpur <elhamaryanpur5@gmail.com>
2025-07-21 20:19:11 +0300

    refactor: clamped libs together and moved middleware to http

--- src/components/http/mod.rs
+++ src/components/http/mod.rs
@@ -5,6 +5,7 @@ pub fn type_definitions() -> String {
     let main = include_str!("http.lua").to_string();
     let server = include_str!("server/server.lua");
     let client = include_str!("client/client.lua");
+    let middleware = include_str!("middleware.lua");
 
-    main + "\n" + server + "\n" + client
+    main + "\n" + server + "\n" + client + "\n" + middleware
 }
--- src/lua_libs/stores.lua
+++ src/lua_libs/stores.lua
@@ -1,5 +1,44 @@
 ---@meta
 
+--MARK: Observable
+
+---An observable object that wraps around the provided data
+---@param val any
+---@return table
+Astra.observable = function(val)
+	local new_observable = {
+		---The original value to be observed
+		value = val,
+		observers = {},
+	}
+
+	---Subscribe to an observable object with a callback function
+	---@param observer function
+	function new_observable:subscribe(observer)
+		if not self.observers[observer] then
+			self.observers[observer] = true
+		end
+	end
+
+	---Unsubscribe a callback function from an observable object
+	---@param observer function
+	function new_observable:unsubscribe(observer)
+		self.observers[observer] = nil
+	end
+
+	---Publish the provided data to all subcribers
+	---@param data function | any
+	function new_observable:publish(data)
+		for k, _ in pairs(self.observers) do
+			k(data)
+		end
+	end
+
+	return new_observable
+end
+
+--MARK: PubSub
+
 local subscriptions = {}
 local subcounter = {}
 
